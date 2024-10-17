60651c7610afc9be68f8ea820444fb9367b13761
Sachinpawar86 <sachinbarange86@gmail.com>
2024-10-17 07:10:54 +0000

    mojito: bringup for AfterlifeOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/bliss_mojito.mk
+    $(LOCAL_DIR)/aosp_mojito.mk
 
 COMMON_LUNCH_CHOICES := \
-    bliss_mojito-user \
-    bliss_mojito-userdebug \
-    bliss_mojito-eng
+    aosp_mojito-user \
+    aosp_mojito-userdebug \
+    aosp_mojito-eng
