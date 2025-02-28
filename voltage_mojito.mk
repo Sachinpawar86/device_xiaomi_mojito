696bb821a189deda1a8d9f14811e8467e04695bb
Sachinpawar86 <sachinbarange86@gmail.com>
2025-02-28 08:37:48 +0530

    mojito: Remove 💀

--- voltage_mojito.mk
+++ voltage_mojito.mk
@@ -16,7 +16,6 @@ $(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Some Build Flags
 TARGET_BOOT_ANIMATION_RES := 1080
-WITH_GMS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := voltage_mojito
