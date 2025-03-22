5298f9eb1fa5f035913d026e15c454c3581c94b6
Sachinpawar07 <sachinbarange86@gmail.com>
2025-03-22 23:49:08 +0530

    mojito: Update Remove Packages

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,43 +5,9 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-    CalculatorGooglePrebuilt \
-    CalendarGooglePrebuilt \
     Chrome \
     Chrome-Stub \
-    GoogleTTS \
-    LocationHistoryPrebuilt \
-    MarkupGoogle \
-    PixelThemesStub \
-    PixelThemesStub2022_and_newer \
-    PrebuiltDeskClockGoogle \
-    SoundPickerPrebuilt \
-    TrichromeLibrary \
-    TrichromeLibrary-Stub \
-    WallpaperEmojiPrebuilt \
-    talkback \
-    AICorePrebuilt \
-    AiWallpapers \
-    BetterBugStub \
-    CarrierLocation \
-    ConfigUpdater \
-    CreativeAssistant \
-    DreamlinerDreamsPrebuilt \
-    DreamlinerPrebuilt \
-    DreamlinerUpdater \
-    DeviceIntelligenceNetworkPrebuilt \
-    DevicePersonalizationPrebuiltPixel2020 \
-    GoogleOneTimeInitializer \
-    KidsSupervisionStub \
-    MaestroPrebuilt \
-    OdadPrebuilt \
-    PartnerSetupPrebuilt \
-    SettingsIntelligenceGooglePrebuilt \
-    TagGoogle \
-    TurboPrebuilt \
-    Velvet \
-    HealthIntelligencePrebuilt \
-    Flipendo
+    BetterBugStub
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
