c26f4f1cefa63776d120dae1560a14d98cc991ba
romgharti <romgharti@gmail.com>
2025-07-18 01:13:07 +0545

    Revert "mojito: Include vendor/debugfs.config"
    This reverts commit 396e15d66c29cf859e0cffb945d76870a1ab2342.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,7 @@ TARGET_SCREEN_DENSITY := 440
 HWUI_COMPILE_FOR_PERF := true
 
 # Kernel
-TARGET_KERNEL_CONFIG := vendor/mojito_defconfig vendor/debugfs.config
+TARGET_KERNEL_CONFIG := vendor/mojito_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/mojito
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     LLVM=1 \
