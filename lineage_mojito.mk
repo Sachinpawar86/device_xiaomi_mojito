a43973c2f5cb0795cc5ecdcd5a17e59afa155af6
Sachinpawar86 <sachinbarange86@gmail.com>
2026-02-05 10:32:33 +0530

    mojito: Bringup @Evolution-X Signed-off-by: Sachin <sachinbarange86@gmail.com>

--- lineage_mojito.mk
+++ lineage_mojito.mk
@@ -11,21 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some LineageOS stuffs
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+# Inherit some Evolution-X stuffs
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Some Build Flags
+# Evolution-X Build Flags
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_ACCORD := false
+TORCH_STR_SUPPORTED := true
 
-# Whether you are compiling being an OFFICIAL Maintainer:
-INFINITY_BUILD_TYPE := OFFICIAL
-
-# Maintainer Name
-INFINITY_MAINTAINER := K4LCHAKRA_&_ROMGHARTI
+# Gapps
+TARGET_USES_PICO_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := infinity_mojito
+PRODUCT_NAME := lineage_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
